6b4ffad7c79d939b54a7b5758eabb9ee18e544cd
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-05-31 16:36:36 +0800

    init propject

--- src/components/__tests__/CounterComponent.test.js
+++ src/components/__tests__/CounterComponent.test.js
@@ -1,13 +1,9 @@
 /* eslint-disable no-undef */
-import { mount, shallowMount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 
 import CounterComponent from "../CounterComponent.vue";
 
 describe("test Counter Component", () => {
-  test("should component is a Vue instance", () => {
-    const wrapper = mount(CounterComponent);
-    expect(wrapper.isVueInstance()).toBeTruthy();
-  });
 
   test("should component has count props", () => {
     const wrapper = shallowMount(CounterComponent, {
