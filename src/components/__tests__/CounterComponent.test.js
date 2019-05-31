30602679e7be2fdcd8f6aa83eb74c3e8da9ce78a
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-05-31 17:14:22 +0800

    optimize test for CounterComponent

--- src/components/__tests__/CounterComponent.test.js
+++ src/components/__tests__/CounterComponent.test.js
@@ -4,13 +4,12 @@ import { shallowMount } from "@vue/test-utils";
 import CounterComponent from "../CounterComponent.vue";
 
 describe("test Counter Component", () => {
-
   test("should component have count prop", () => {
     const wrapper = shallowMount(CounterComponent, {
       propsData: {
         count: 6
       }
     });
-    expect(wrapper.find(".counter").text()).toBe("6");
+    expect(wrapper.text()).toBe("6");
   });
 });
